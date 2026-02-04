93ed0f0826d5cb240d3ab9b972e2da8f6c6c5778
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-04 02:09:36 +0000

    resolving wsgi.py

--- food_ord_app_be/wsgi.py
+++ food_ord_app_be/wsgi.py
@@ -1,7 +1,15 @@
+# import os
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_ord_app_be.settings')
+
+# app = get_wsgi_application()     # 👈 REQUIRED BY VERCEL
+# handler = app                   # 👈 ALSO SAFE
+
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_ord_app_be.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_ord_app_be.settings")
 
-app = get_wsgi_application()     # 👈 REQUIRED BY VERCEL
-handler = app                   # 👈 ALSO SAFE
+app = get_wsgi_application()
+handler = app   # 👈 THIS LINE IS THE FIX
