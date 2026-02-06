bc88562612a8ab71a50475978eb1983a34eedd71
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-06 18:37:39 +0000

    wsgi.py changes

--- food_ord_app_be/wsgi.py
+++ food_ord_app_be/wsgi.py
@@ -5,12 +5,10 @@
 
 # app = get_wsgi_application()     # 👈 REQUIRED BY VERCEL
 # handler = app                   # 👈 ALSO SAFE
-
+    
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_ord_app_be.settings")
 
-app = get_wsgi_application()
-handler = app  # REQUIRED for Render.com and Vercel
-    
+application = get_wsgi_application()
