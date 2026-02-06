6447320a1abc15abb0498875d9417cb955f9dc3b
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-06 17:59:17 +0000

    correcting asgi to wsgi

--- food_ord_app_be/wsgi.py
+++ food_ord_app_be/wsgi.py
@@ -7,7 +7,7 @@
 # handler = app                   # 👈 ALSO SAFE
 
 import os
-from django.core.asgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_ord_app_be.settings")
 
