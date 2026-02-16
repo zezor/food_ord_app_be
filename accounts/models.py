1700ded5b5663f3f716cef9befb297b47726cecb
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-16 00:41:21 +0000

    fixing internal error relating to custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,13 @@ from departments.models import Department
 
 
 class CustomUserManager(BaseUserManager):
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        phone_number = extra_fields.get("phone_number", "")
-        department = extra_fields.get("department", None)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -20,6 +19,11 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self.create_user(email, password, **extra_fields)
 
@@ -35,14 +39,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         on_delete=models.SET_NULL
     )
 
-
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    # ✅ REQUIRED BY DJANGO
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []   # ← THIS IS THE MISSING LINE
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
