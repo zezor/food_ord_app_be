8a815ed048c099e1b79d419fae6161da1658ea0e
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-16 00:52:18 +0000

    still fixing admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,34 +3,36 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
 
 
-@admin.register(CustomUser)
-class UserAdmin(BaseUserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
 
     list_display = (
-        'email',
-        'full_name',
-        'is_staff',
-        'is_active',
-        'date_joined',
-        'department',
+        "email",
+        "full_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "department",
     )
 
-    search_fields = ('email', 'full_name')
-    ordering = ('email',)
+    list_filter = ("is_staff", "is_active")
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name', 'department')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("full_name", "phone_number", "department")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'full_name', 'department', 'password1', 'password2', 'is_staff', 'is_active'),
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
         }),
     )
 
+    search_fields = ("email", "full_name")
+    ordering = ("email",)
 
+
+admin.site.register(CustomUser, CustomUserAdmin)
