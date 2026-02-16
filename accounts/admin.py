795a9e1322837b1d121981d4803fbdfeb7e18ed0
Emmanuel Nyamekye Nti <kemma2993@gmail.com>
2026-02-16 00:33:16 +0000

    fixing accounts admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
-from .models import CustomUser, CustomUserManager
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name', 'is_staff', 'is_active', 'date_joined', 'department')
+
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+
+    list_display = (
+        'email',
+        'full_name',
+        'is_staff',
+        'is_active',
+        'date_joined',
+        'department',
+    )
+
     search_fields = ('email', 'full_name')
     ordering = ('email',)
-admin.site.register(CustomUser, UserAdmin)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'department')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'department', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
 
 
